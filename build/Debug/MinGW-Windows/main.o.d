4911632e837f7d90aa2c4b09b15e8d0fc7e8b054
Natanael Zimmer <nrzimmer@gmail.com>
2014-04-02 15:48:43 -0300

    adicionados defines de endereços

---
+++ build/Debug/MinGW-Windows/main.o.d
@@ -0,0 +1,37 @@
+build/Debug/MinGW-Windows/main.o: main.cpp util.h M6507.h \
+ M6507_Reg_Operations.h M6507_Load_Opcodes.h M6507_Store_Opcodes.h \
+ M6507_Arithmetic_Opcodes.h M6507_Inc_Dec_Opcodes.h \
+ M6507_Shit_Rotate_Opcodes.h M6507_Logic_Opcodes.h \
+ M6507_CMP_Test_Bits_Opcodes.h M6507_Branch_Opcodes.h \
+ M6507_Sub_Jump_Opcodes.h M6507_Transfer_Opcodes.h M6507_Stack_Opcodes.h \
+ M6507_Set_Clear_Misc_Opcodes.h
+
+util.h:
+
+M6507.h:
+
+M6507_Reg_Operations.h:
+
+M6507_Load_Opcodes.h:
+
+M6507_Store_Opcodes.h:
+
+M6507_Arithmetic_Opcodes.h:
+
+M6507_Inc_Dec_Opcodes.h:
+
+M6507_Shit_Rotate_Opcodes.h:
+
+M6507_Logic_Opcodes.h:
+
+M6507_CMP_Test_Bits_Opcodes.h:
+
+M6507_Branch_Opcodes.h:
+
+M6507_Sub_Jump_Opcodes.h:
+
+M6507_Transfer_Opcodes.h:
+
+M6507_Stack_Opcodes.h:
+
+M6507_Set_Clear_Misc_Opcodes.h:
